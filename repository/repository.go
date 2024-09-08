82dcc19c48441b4c0d66c747206e258c7cc00756
Antonio Perez <antonio.perez@gametime.co>
2024-09-08 17:36:56 -0600

    feat: create repository/repository.go file

---
+++ repository/repository.go
@@ -0,0 +1,76 @@
+package repository
+
+import "github.com/przant/zipcodes-api/models"
+
+type ZipcodesRepo interface {
+	FetchByZipcode(zipcode string) (*models.Zipcode, error)
+	FetchByCounty(county string) ([]models.Zipcode, error)
+	FetchByStateCounty(state, county string) ([]models.Zipcode, error)
+	FetchByStateCity(state, city string) ([]models.Zipcode, error)
+	FecthByCountyCity(county, city string) ([]models.Zipcode, error)
+}
+
+type ZipcodesService struct {
+	repo ZipcodesRepo
+}
+
+var (
+	instance *ZipcodesService
+)
+
+func NewZipcodesService(zr ZipcodesRepo) {
+	if instance == nil {
+		instance = &ZipcodesService{
+			repo: zr,
+		}
+	}
+}
+
+func GetZipcodesService() *ZipcodesService {
+	return instance
+}
+
+func (zs *ZipcodesService) FetchByZipcode(zipcode string) (*models.Zipcode, error) {
+	zcs, err := zs.repo.FetchByZipcode(zipcode)
+	if err != nil {
+		return nil, err
+	}
+
+	return zcs, nil
+}
+
+func (zs *ZipcodesService) FetchByCounty(county string) ([]models.Zipcode, error) {
+	zcs, err := zs.repo.FetchByCounty(county)
+	if err != nil {
+		return nil, err
+	}
+
+	return zcs, nil
+}
+
+func (zs *ZipcodesService) FetchByStateCounty(state, county string) ([]models.Zipcode, error) {
+	zcs, err := zs.repo.FetchByStateCounty(state, county)
+	if err != nil {
+		return nil, err
+	}
+
+	return zcs, nil
+}
+
+func (zs *ZipcodesService) FetchByStateCity(state, city string) ([]models.Zipcode, error) {
+	zcs, err := zs.repo.FetchByStateCity(state, city)
+	if err != nil {
+		return nil, err
+	}
+
+	return zcs, nil
+}
+
+func (zs *ZipcodesService) FecthByCountyCity(county, city string) ([]models.Zipcode, error) {
+	zcs, err := zs.repo.FetchByStateCity(county, city)
+	if err != nil {
+		return nil, err
+	}
+
+	return zcs, nil
+}
