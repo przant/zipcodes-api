5df28d12761942f43315bab21a15e672ab181de5
Antonio Perez <antonio.perez@gametime.co>
2024-09-08 22:35:20 -0600

    refactor: update methods' logic for fetching info

--- database/local/local_repo.go
+++ database/local/local_repo.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/przant/zipcodes-api/models"
 )
@@ -13,11 +14,7 @@ type LocalDBRepo struct {
 func NewLocalDBRepo() (*LocalDBRepo, error) {
 	lr := &LocalDBRepo{
 		db: &LocalDB{
-			ZipcodesTable:    make(map[string]map[string]string),
-			CountyTable:      make(map[string]map[string]map[string]string),
-			StateCountyTable: make(map[string]map[string]map[string]map[string]string),
-			StateCityTable:   make(map[string]map[string]map[string]map[string]string),
-			CountyCityTable:  make(map[string]map[string]map[string]map[string]string),
+			StateTable: make(map[string]map[string]map[string]string),
 		},
 	}
 
@@ -33,101 +30,127 @@ func (lr *LocalDBRepo) Close() error {
 }
 
 func (lr *LocalDBRepo) FetchByZipcode(zipcode string) (*models.Zipcode, error) {
-	if _, exist := lr.db.ZipcodesTable[zipcode]; !exist {
-		return nil, fmt.Errorf("the zipcode %q does not exist in the database", zipcode)
+	for st, zips := range lr.db.StateTable {
+		if _, exist := zips[zipcode]; exist {
+			return &models.Zipcode{
+				State:     st,
+				Zipcode:   zipcode,
+				StateFIPS: zips[zipcode]["StateFIPS"],
+				StateAbbr: zips[zipcode]["StateAbbr"],
+				County:    zips[zipcode]["CountyName"],
+				City:      zips[zipcode]["CityName"],
+			}, nil
+		}
 	}
 
-	return &models.Zipcode{
-		Zipcode:   zipcode,
-		StateFIPS: lr.db.ZipcodesTable[zipcode]["StateFIPS"],
-		StateAbbr: lr.db.ZipcodesTable[zipcode]["StateAbbr"],
-		State:     lr.db.ZipcodesTable[zipcode]["StateName"],
-		County:    lr.db.ZipcodesTable[zipcode]["CountyName"],
-		City:      lr.db.ZipcodesTable[zipcode]["CityName"],
-	}, nil
+	return nil, fmt.Errorf("the zipcode %q does not exist in the database", zipcode)
 }
 
 func (lr *LocalDBRepo) FetchByCounty(county string) ([]models.Zipcode, error) {
-	if _, exist := lr.db.CountyTable[county]; !exist {
-		return nil, fmt.Errorf("the county %q does not exist in the database", county)
+	rs := make([]models.Zipcode, 0)
+
+	for st, zips := range lr.db.StateTable {
+		for zipcode, attrs := range zips {
+			if strings.EqualFold(attrs["CountyName"], county) {
+				r := models.Zipcode{
+					State:     st,
+					Zipcode:   zipcode,
+					County:    county,
+					StateFIPS: attrs["StateFIPS"],
+					City:      attrs["CityName"],
+				}
+
+				rs = append(rs, r)
+			}
+		}
 	}
 
-	rs := make([]models.Zipcode, 0)
-	for zc, attrs := range lr.db.CountyTable[county] {
-		r := models.Zipcode{
-			Zipcode:   zc,
-			County:    county,
-			StateFIPS: attrs["StateFIPS"],
-			StateAbbr: attrs["StateAbbr"],
-			State:     attrs["StateName"],
-			City:      attrs["CityName"],
-		}
-
-		rs = append(rs, r)
+	if len(rs) == 0 {
+		return nil, fmt.Errorf("the county %q does not exist in the database", county)
 	}
 
 	return rs, nil
 }
 
 func (lr *LocalDBRepo) FetchByStateCounty(state, county string) ([]models.Zipcode, error) {
-	if _, exist := lr.db.StateCountyTable[state][county]; !exist {
+	rs := make([]models.Zipcode, 0)
+
+	for st, zips := range lr.db.StateTable {
+		if strings.EqualFold(st, state) {
+			for zip, attrs := range zips {
+				if strings.EqualFold(attrs["CountyName"], county) {
+					r := models.Zipcode{
+						State:     state,
+						Zipcode:   zip,
+						County:    county,
+						StateFIPS: attrs["StateFIPS"],
+						StateAbbr: attrs["StateAbbr"],
+						City:      attrs["CityName"],
+					}
+
+					rs = append(rs, r)
+				}
+			}
+		}
+	}
+
+	if len(rs) == 0 {
 		return nil, fmt.Errorf("the state-county pair %q does not exist in the database", state+"-"+county)
 	}
 
-	rs := make([]models.Zipcode, 0)
-
-	for zc, attrs := range lr.db.StateCountyTable[state][county] {
-		r := models.Zipcode{
-			Zipcode:   zc,
-			State:     state,
-			County:    county,
-			StateFIPS: attrs["StateFIPS"],
-			StateAbbr: attrs["StateAbbr"],
-			City:      attrs["CityName"],
-		}
-		rs = append(rs, r)
-	}
 	return rs, nil
 }
 
 func (lr *LocalDBRepo) FetchByStateCity(state, city string) ([]models.Zipcode, error) {
-	if _, exist := lr.db.StateCityTable[state][city]; !exist {
-		return nil, fmt.Errorf("the state-city pair %q does not exist in the database", state+"-"+city)
-	}
-
 	rs := make([]models.Zipcode, 0)
 
-	for zc, attrs := range lr.db.StateCountyTable[state][city] {
-		r := models.Zipcode{
-			Zipcode:   zc,
-			State:     state,
-			City:      city,
-			StateFIPS: attrs["StateFIPS"],
-			StateAbbr: attrs["StateAbbr"],
-			County:    attrs["CityName"],
+	for st, zips := range lr.db.StateTable {
+		if strings.EqualFold(st, state) {
+			for zip, attrs := range zips {
+				if strings.EqualFold(attrs["CityName"], city) {
+					r := models.Zipcode{
+						State:     state,
+						Zipcode:   zip,
+						City:      city,
+						StateFIPS: attrs["StateFIPS"],
+						StateAbbr: attrs["StateAbbr"],
+						County:    attrs["CountyName"],
+					}
+
+					rs = append(rs, r)
+				}
+			}
 		}
-		rs = append(rs, r)
+	}
+
+	if len(rs) == 0 {
+		return nil, fmt.Errorf("the state-city pair %q does not exist in the database", state+"-"+city)
 	}
 	return rs, nil
 }
 
-func (lr *LocalDBRepo) FecthByCountyCity(county, city string) ([]models.Zipcode, error) {
-	if _, exist := lr.db.StateCityTable[county][city]; !exist {
-		return nil, fmt.Errorf("the state-city pair %q does not exist in the database", county+"-"+city)
-	}
-
+func (lr *LocalDBRepo) FetchByCountyCity(county, city string) ([]models.Zipcode, error) {
 	rs := make([]models.Zipcode, 0)
 
-	for zc, attrs := range lr.db.StateCountyTable[county][city] {
-		r := models.Zipcode{
-			Zipcode:   zc,
-			City:      city,
-			County:    county,
-			StateFIPS: attrs["StateFIPS"],
-			StateAbbr: attrs["StateAbbr"],
-			State:     attrs["StateName"],
+	for st, zips := range lr.db.StateTable {
+		for zip, attrs := range zips {
+			if strings.EqualFold(attrs["CountyName"], county) && strings.EqualFold(attrs["CityName"], city) {
+				r := models.Zipcode{
+					State:     st,
+					Zipcode:   zip,
+					City:      city,
+					County:    county,
+					StateFIPS: attrs["StateFIPS"],
+					StateAbbr: attrs["StateAbbr"],
+				}
+
+				rs = append(rs, r)
+			}
 		}
-		rs = append(rs, r)
+	}
+
+	if len(rs) == 0 {
+		return nil, fmt.Errorf("the county-city pair %q does not exist in the database", county+"-"+city)
 	}
 	return rs, nil
 }
