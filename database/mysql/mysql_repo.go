b7f7cbf1c6a97fdce94519e791c65a992e99899d
Antonio Perez <antonio.perez@gametime.co>
2024-09-09 05:48:30 -0600

    feat: add database/mysql/mysql_repo.go file

---
+++ database/mysql/mysql_repo.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+type MySQLRepo struct {
+	db *sql.DB
+}
+
+func NewMySQLRepo() (*MySQLRepo, error) {
+	err := godotenv.Load("mysql.env")
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := mysql.Config{
+		User:   os.Getenv("MYSQL_USER"),
+		Passwd: os.Getenv("MYSQL_PASSWORD"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "us_zipcodes",
+	}
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &MySQLRepo{db: db}, err
+}
+
+func (db *MySQLRepo) Close() {
+	if err := db.db.Close(); err != nil {
+		log.Fatalf("while closing the MySQL database connection: %s", err)
+	}
+}
