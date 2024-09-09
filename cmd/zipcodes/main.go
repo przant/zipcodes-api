ca29a622a06c097cbab695bbe5ef5bc43cfcd660
Antonio Perez <antonio.perez@gametime.co>
2024-09-09 06:57:25 -0600

    test: check updates behavior

--- cmd/zipcodes/main.go
+++ cmd/zipcodes/main.go
@@ -3,28 +3,17 @@ package main
 import (
 	"log"
 
-	db "github.com/przant/zipcodes-api/database/local"
-	"github.com/przant/zipcodes-api/http/rest"
-	repo "github.com/przant/zipcodes-api/repository"
+	db "github.com/przant/zipcodes-api/database/mysql"
 )
 
 func main() {
 
-	db, err := db.NewLocalDBRepo()
+	db, err := db.NewMySQLRepo()
 	if err != nil {
 		log.Fatalf("while connecting to the db: %s", err)
 	}
+
+	log.Print("Conexion attempt succesfully to the mysql database")
+
 	defer db.Close()
-
-	repo.NewZipcodesService(db)
-
-	api := rest.NewAPI()
-
-	api.GET("/zipcodes/:zipcode", rest.GetByZipcode)
-	api.GET("/counties/:county", rest.GetByCounty)
-	api.GET("/counties/:county/cities/:city", rest.GetByCountyCity)
-	api.GET("/states/:state/counties/:county", rest.GetByStateCounty)
-	api.GET("/states/:state/cities/:city:", rest.GetByStateCity)
-
-	api.Logger.Fatal(api.Start(rest.PORT))
 }
