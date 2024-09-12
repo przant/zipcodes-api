ae94da96bc1c41aee04a1343dfa1760905a0d350
Antonio Perez <antonio.perez@gametime.co>
2024-09-12 08:28:41 -0600

    refactor: update cdm/zipcodes/main.go to use the MySQLRepo service

--- cmd/zipcodes/main.go
+++ cmd/zipcodes/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	db "github.com/przant/zipcodes-api/database/mysql"
+	"github.com/przant/zipcodes-api/http/rest"
+	repo "github.com/przant/zipcodes-api/repository"
 )
 
 func main() {
@@ -12,8 +14,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("while connecting to the db: %s", err)
 	}
-
-	log.Print("Conexion attempt succesfully to the mysql database")
-
 	defer db.Close()
+
+	repo.NewZipcodesService(db)
+
+	api := rest.NewAPI()
+	api.GET("/zipcodes/:zipcode", rest.GetByZipcode)
+	api.GET("/counties/:county", rest.GetByCounty)
+	api.GET("/states/:state/counties/:county", rest.GetByStateCounty)
+	api.GET("/states/:state/cities/:city", rest.GetByStateCity)
+	api.GET("/counties/:county/cities/:city", rest.GetByCountyCity)
+
+	api.Logger.Fatal(api.Start(rest.PORT))
 }
