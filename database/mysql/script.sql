6c3c6094e15ff37cf8752ddc32068c12ee9ce2e0
Antonio Perez <antonio.perez@gametime.co>
2024-09-12 00:15:39 -0600

    refactor: update zipcodes table attributes definitions

--- database/mysql/script.sql
+++ database/mysql/script.sql
@@ -6,10 +6,10 @@ USE us_zipcodes;
 DROP TABLE IF EXISTS zipcodes;
 CREATE TABLE zipcodes(
     id         INTEGER AUTO_INCREMENT NOT NULL,
-    state      VARCHAR(15) NOT NULL,
+    state      VARCHAR(25) NOT NULL,
     state_abbr CHAR(2) NOT NULL,
-    zipcode    INTEGER NOT NULL,
-    county     VARCHAR(25) NOT NULL,
-    city       VARCHAR(25) NOT NULL,
+    zipcode    VARCHAR(7) NOT NULL,
+    county     VARCHAR(50) NOT NULL,
+    city       VARCHAR(35) NOT NULL,
     PRIMARY KEY (id)
 );
