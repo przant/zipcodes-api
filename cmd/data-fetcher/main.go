627416222bccf27c4f151f02351211d11343b44b
Antonio Perez <antonio.perez@gametime.co>
2024-09-17 13:53:17 -0600

    feat: enable dynamic database-table and database-collection from .env file

--- cmd/data-fetcher/main.go
+++ cmd/data-fetcher/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/przant/zipcodes-api/models"
 	"github.com/przant/zipcodes-api/utils"
 )
@@ -16,8 +17,8 @@ var (
 )
 
 const (
-	INSERT_MYSQL = "USE us_zipcodes;\nINSERT INTO zipcodes(state, state_abbr, zipcode, county, city) VALUES"
-	INSERT_MONGO = "const conn = new Mongo();\n\nconst db = conn.getDB(\"us_zipcodes\");\n\ndb.zipcodes.drop();\n"
+	INSERT_MYSQL = "USE %s;\nINSERT INTO %s(state, state_abbr, zipcode, county, city) VALUES\n"
+	INSERT_MONGO = "const conn = new Mongo();\n\nconst db = conn.getDB(\"%s\");\n\ndb.%s.drop();\n"
 	MYSQLDB      = "mysql"
 	MONGODB      = "mongo"
 )
@@ -35,6 +36,11 @@ func main() {
 		log.Fatalf("while feching the zipcodes data: %s", err)
 	}
 
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("while loading %q file: %s", ".env", err)
+	}
+
 	switch {
 	case dbType == MYSQLDB:
 		createMySQlInsert(zips)
@@ -44,18 +50,20 @@ func main() {
 }
 
 func createMySQlInsert(zips []models.Zipcode) {
-	file, err := os.OpenFile("insert.sql", os.O_RDWR|os.O_CREATE, 0650)
+	file, err := os.OpenFile("insert.sh", os.O_RDWR|os.O_CREATE, 0650)
 	if err != nil {
 		log.Fatalf("while creating the file %q: %s", "insert.sql", err)
 	}
 	defer file.Close()
 
-	fmt.Fprintf(file, "%s\n", INSERT_MYSQL)
+	fmt.Fprintf(file, "SQL=$(cat <<-EOF\n")
+	fmt.Fprintf(file, INSERT_MYSQL, os.Getenv("MYSQL_DATABASE"), os.Getenv("MYSQL_TABLE"))
 	for _, v := range zips[1 : len(zips)-1] {
 		fmt.Fprintf(file, "(%q,%q,%q,%q,%q),\n", v.State, v.StateAbbr, v.Zipcode, v.County, v.City)
 	}
 	v := zips[len(zips)-1]
 	fmt.Fprintf(file, "(%q,%q,%q,%q,%q);\n", v.State, v.StateAbbr, v.Zipcode, v.County, v.City)
+	fmt.Fprintf(file, "EOF\n)\necho $SQL | mysql -u$MYSQL_USER -p$MYSQL_PASSWORD\n")
 }
 
 func createMONGOInsert(zips []models.Zipcode) {
@@ -65,7 +73,8 @@ func createMONGOInsert(zips []models.Zipcode) {
 	}
 	defer file.Close()
 
-	fmt.Fprintf(file, "%s\ndb.zipcodes.insertMany(", INSERT_MONGO)
+	fmt.Fprintf(file, INSERT_MONGO, os.Getenv("MONGODB_DATABASE"), os.Getenv("MONGODB_COLLECTION"))
+	fmt.Fprintf(file, "\ndb.%s.insertMany(", os.Getenv("MONGODB_COLLECTION"))
 	bs, err := json.MarshalIndent(zips[1:], "", "    ")
 	if err != nil {
 		log.Fatalf("while marshaling the data to insert: %s", err)
